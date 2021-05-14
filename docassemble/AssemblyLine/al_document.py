1e0356378987fdce76896224f23d91a42d5539d5
Lily  Yang <20231649+purplesky2016@users.noreply.github.com>
2021-05-14 16:22:19 -0400

    Add back the bottom  <div class="al_email_container">

--- docassemble/AssemblyLine/al_document.py
+++ docassemble/AssemblyLine/al_document.py
@@ -699,7 +699,7 @@ class ALDocumentBundle(DAList):
 
     return_str = '''
   <div class="al_send_bundle '''+name+'''" id="al_send_bundle_'''+name+'''" name="al_send_bundle_'''+name+'''">
-  <h5 id="al_doc_email_header">Get a copy of the documents in email</h5>      
+  <h5 id="al_doc_email_header">Get a copy of the documents in email</h5>  
     '''
     if show_editable_checkbox:
       return_str += '''
@@ -711,6 +711,14 @@ class ALDocumentBundle(DAList):
   </div>
   '''
     return_str += "</div>"
+    return_str += '''    
+  <div class="al_email_container">
+  <span class="al_email_address '''+name+''' form-group row da-field-container da-field-container-datatype-email">
+    <label for="'''+al_email_input_id+'''" class="al_doc_email col-form-label da-form-label datext-right">Email address</label>
+    <input value="''' + (user_info().email if user_logged_in() else '') + '''" alt="Input box" class="form-control" type="email" size="35" name="'''+al_email_input_id+'''" id="'''+al_email_input_id+'''">
+  </span>''' + action_button_html(javascript_string, label="Send", icon="envelope", color="primary", size="md", classname="al_send_email_button", id_tag=al_send_button_id) + "\n" + '''
+  </div></div>
+  '''
     return return_str
 
   def send_email(self, to:any=None, key:str='final', editable:bool=False, template:any=None, **kwargs) -> bool:
