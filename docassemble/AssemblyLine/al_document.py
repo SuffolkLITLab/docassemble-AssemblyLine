804e3dbc0021581dbf5b48b810e380232e63d709
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2022-01-02 20:59:23 -0500

    Added to the documentation that None can be returned from as_pdf

--- docassemble/AssemblyLine/al_document.py
+++ docassemble/AssemblyLine/al_document.py
@@ -1,7 +1,7 @@
 import re
 import os
 import mimetypes
-from typing import Any, Dict, List, Union, Callable
+from typing import Any, Dict, List, Union, Callable, Optional
 from docassemble.base.util import log, word, DADict, DAList, DAObject, DAFile, DAFileCollection, DAFileList, defined, value, pdf_concatenate, zip_file, DAOrderedDict, action_button_html, include_docx_template, user_logged_in, user_info, send_email, docx_concatenate, get_config, space_to_underscore, DAStaticFile, alpha
 
 __all__ = ['ALAddendumField',
@@ -774,7 +774,8 @@ class ALDocumentBundle(DAList):
     # Pre-cache some DALazyTemplates we set up to aid translation that won't
     # vary at runtime    
 
-  def as_pdf(self, key:str='final', refresh:bool=True) -> DAFile:
+  def as_pdf(self, key:str='final', refresh:bool=True) -> Optional[DAFile]:
+    """Returns the Bundle as a single PDF DAFile, or None if none of the documents are enabled."""
     safe_key = space_to_underscore(key)
 
     log_if_debug('Calling the as_pdf() method for bundle ' + str(self.title))
