e4ee65ce77b7b292c1d11295b28bf1e1df183d95
Anonymous User <52798256+plocket@users.noreply.github.com>
2023-01-18 12:05:12 -0500

    Handle screens at <=500px, validation errors

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='docassemble.AssemblyLine',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.ALToolbox>=0.6.0', 'docassemble.GithubFeedbackForm>=0.2.0'],
+      install_requires=['docassemble.ALToolbox>=0.6.2', 'docassemble.GithubFeedbackForm>=0.2.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/AssemblyLine/', package='docassemble.AssemblyLine'),
      )
