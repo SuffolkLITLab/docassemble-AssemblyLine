b754b29186bf91f508a41a5cf82673170a1561e1
plocket <52798256+plocket@users.noreply.github.com>
2026-08-14 13:21:12 -0400

    Correct session.py typo to "dictionary"
    I'm unfamiliar with SQL and I'm curious about where this value is being used, but if it is being used the spelling may matter?

--- docassemble/AssemblyLine/sessions.py
+++ docassemble/AssemblyLine/sessions.py
@@ -398,7 +398,7 @@ def get_saved_interview_list(
 
     query_draft = """
         WITH filtered_userdict AS (
-            SELECT userdict.indexno as indexno, userdict.key as key, userdict.dictionary as dictinory, userdict.modtime as modtime, userdict.user_id as user_id, userdict.filename as filename
+            SELECT userdict.indexno as indexno, userdict.key as key, userdict.dictionary as dictionary, userdict.modtime as modtime, userdict.user_id as user_id, userdict.filename as filename
             FROM userdict JOIN userdictkeys ON userdict.key = userdictkeys.key
             -- userdictkeys.user_id has an index, that's why we need the join
             WHERE (userdictkeys.user_id = :user_id or :user_id is null)
@@ -616,7 +616,7 @@ def find_matching_sessions(
 
     get_sessions_query = text(f"""
         WITH filtered_userdict AS (
-            SELECT userdict.indexno as indexno, userdict.key as key, userdict.dictionary as dictinory, userdict.modtime as modtime, userdict.user_id as user_id, userdict.filename as filename
+            SELECT userdict.indexno as indexno, userdict.key as key, userdict.dictionary as dictionary, userdict.modtime as modtime, userdict.user_id as user_id, userdict.filename as filename
             FROM userdict JOIN userdictkeys ON userdict.key = userdictkeys.key
             -- userdictkeys.user_id has an index, that's why we need the join
             WHERE (userdictkeys.user_id = :user_id or :user_id is null)
